fe73251e15ffb0d2bef34b4427de813621678ed9
andreyka26_ <b.andriy.b2000@gmail.com>
2021-08-23 18:09:24 +0300

    fixed async & refactored

--- json/migrator.js
+++ json/migrator.js
@@ -10,7 +10,7 @@ const SETTLEMENT = 'SETTLEMENT';
 const CAPITAL_CITY = 'CAPITAL_CITY';
 const COUNTRY = 'COUNTRY';
 
-let types = new Map();
+const types = new Map();
 types.set(VILLAGE, VILLAGE); // село
 types.set(DISTRICT, DISTRICT); // район
 types.set(STATE, STATE); // область
@@ -20,10 +20,10 @@ types.set(SETTLEMENT, SETTLEMENT); // село
 types.set(CAPITAL_CITY, CAPITAL_CITY); // місто
 types.set(COUNTRY, COUNTRY); // країна
 
-let _itemsMap = new Map(); // all entities id -> item
+const _itemsMap = new Map(); // all entities id -> item
 
-let _countriesNameIdMap = new Map(); // name -> id
-let _regionsNameIdMap = new Map(); // name -> id
+const _countriesNameIdMap = new Map(); // name -> id
+const _regionsNameIdMap = new Map(); // name -> id
 
 const _client = new Client({
   user: 'root',
@@ -35,22 +35,22 @@ const _client = new Client({
 
 _client.connect()
 
-fs.readFile('locations.json', 'utf8' , async (err, data) => {
+fs.readFile('locations.json', 'utf8', async (err, data) => {
   if (err) {
     console.error(err);
     return;
   }
 
-  let items = JSON.parse(data);
+  const items = JSON.parse(data);
 
   console.log('add countries')
   //preset hashtable for quicker search and entities with no parent_id
   //during that process add and set addedCountries hashmap
-  for (let item of items) {
+  for (const item of items) {
     _itemsMap.set(item.id, item);
 
     if (item.type == COUNTRY) {
-      let countryId = await addCountry(item);
+      const countryId = await addCountry(item);
       _countriesNameIdMap.set(item.name.en, countryId);
     }
   }
@@ -61,100 +61,110 @@ fs.readFile('locations.json', 'utf8' , async (err, data) => {
   console.log('added regions')
 
   console.log('add cities')
-  addCities(items);
+  await addCities(items);
   console.log('added cities')
 })
 
 async function addRegions(items) {
-  for (let item of items) {
-    if (item.type == STATE) {
-      let countryId = findCountryForEntity(item);
+  const filteredItems = items.filter(({ type }) => type == STATE);
 
-      let regionId = await addRegion(item, countryId);
-      _regionsNameIdMap.set(item.name.en, regionId);
-    }
-  }
+  await Promise.all(filteredItems.map(async (region) => {
+    const countryId = findCountryForEntity(region);
+    const regionId = await addRegion(region, countryId);
+
+    console.log(`added region ${region.name.uk}`);
+
+    _regionsNameIdMap.set(region.name.en, regionId);
+  }));
 }
 
 async function addCities(items) {
-  for (let item of items) {
-    if (item.type == VILLAGE || item.type == CITY || item.type == URBAN || item.type == SETTLEMENT || item.type == CAPITAL_CITY) {
-      try{
-        let regionId = findRegionForEntity(item);
-        let countryId = findCountryForEntity(item);
-        addCity(item, regionId, countryId);
-      } catch(err) {
-        console.log(err);
-      }
+  const cities = items.filter(({ type }) => isCityType(type));
+
+  await Promise.all(cities.map(async (city) => {
+    try {
+      const regionId = findRegionForEntity(city);
+      const countryId = findCountryForEntity(city);
+      await addCity(city, regionId, countryId);
+
+      console.log(`added city ${city.name.uk}`);
+    } catch (err) {
+      console.log(err);
     }
-  }
+  }));
+}
+
+function isCityType(type) {
+  return type == VILLAGE || type == CITY || type == URBAN || type == SETTLEMENT || type == CAPITAL_CITY;
 }
 
 function findRegionForEntity(entity) {
-  if (!entity.parent_id || !_itemsMap.has(entity.parent_id))
+  if (!entity.parent_id || !_itemsMap.has(entity.parent_id)) {
     throw new Error(`cannot find parent entity for ${JSON.stringify(entity)}`);
+  }
 
-    let parent = _itemsMap.get(entity.parent_id);
+  const parent = _itemsMap.get(entity.parent_id);
 
-    if (parent.type == STATE) {
-      let addedRegionId = _regionsNameIdMap.get(parent.name.en);
-      return addedRegionId;
-    }
+  if (parent.type == STATE) {
+    const addedRegionId = _regionsNameIdMap.get(parent.name.en);
+    return addedRegionId;
+  }
 
-    return findRegionForEntity(parent);
+  return findRegionForEntity(parent);
 }
 
 function findCountryForEntity(entity) {
-  if (!entity.parent_id || !_itemsMap.has(entity.parent_id))
+  if (!entity.parent_id || !_itemsMap.has(entity.parent_id)) {
     throw new Error(`cannot find parent entity for ${JSON.stringify(entity)}`);
+  }
 
-    let parent = _itemsMap.get(entity.parent_id);
+  const parent = _itemsMap.get(entity.parent_id);
 
-    if (parent.type == COUNTRY) {
-      let addedCountryId = _countriesNameIdMap.get(parent.name.en);
-      return addedCountryId;
-    }
+  if (parent.type == COUNTRY) {
+    const addedCountryId = _countriesNameIdMap.get(parent.name.en);
+    return addedCountryId;
+  }
 
-    return findCountryForEntity(parent);
+  return findCountryForEntity(parent);
 }
 
 async function addCountry(country) {
-  let query = "INSERT INTO countries(title_ru, title_ua, title_en, created_date_time) VALUES($1, $2, $3, $4)  RETURNING *";
-  let values = [country.name.ru, country.name.uk, country.name.en,  getPostgresDateNow()];
+  const query = "INSERT INTO countries(title_ru, title_ua, title_en, created_date_time) VALUES($1, $2, $3, $4)  RETURNING *";
+  const values = [country.name.ru, country.name.uk, country.name.en, getPostgresDateNow()];
 
   try {
-    let res = await _client.query(query, values)
+    const res = await _client.query(query, values)
     return res.rows[0].id;
 
   } catch (err) {
-    console.log(err.stack)
+    console.log(err.stack);
   }
 }
 
 async function addRegion(region, countryId) {
-  let query = "INSERT INTO regions(country_id, title_ru, title_ua, title_en, created_date_time) VALUES($1, $2, $3, $4, $5)  RETURNING *";
-  let values = [countryId, region.name.ru, region.name.uk, region.name.en,  getPostgresDateNow()];
+  const query = "INSERT INTO regions(country_id, title_ru, title_ua, title_en, created_date_time) VALUES($1, $2, $3, $4, $5)  RETURNING *";
+  const values = [countryId, region.name.ru, region.name.uk, region.name.en, getPostgresDateNow()];
 
   try {
-    let res = await _client.query(query, values)
+    const res = await _client.query(query, values);
     return res.rows[0].id;
   } catch (err) {
-    console.log(err.stack)
+    console.log(err.stack);
   }
 }
 
 async function addCity(city, regionId, countryId) {
-  let query = "INSERT INTO cities(country_id, region_id, title_ru, title_ua, title_en, created_date_time) VALUES($1, $2, $3, $4, $5, $6)  RETURNING *";
-  let values = [countryId, regionId, city.name.ru, city.name.uk, city.name.en, getPostgresDateNow()];
+  const query = "INSERT INTO cities(country_id, region_id, title_ru, title_ua, title_en, created_date_time) VALUES($1, $2, $3, $4, $5, $6)  RETURNING *";
+  const values = [countryId, regionId, city.name.ru, city.name.uk, city.name.en, getPostgresDateNow()];
 
   try {
-    let res = await _client.query(query, values)
+    const res = await _client.query(query, values)
     return res.rows[0].id;
   } catch (err) {
-    console.log(err.stack)
+    console.log(err.stack);
   }
 }
 
 function getPostgresDateNow() {
-  return new Date().toISOString().slice(0,10);
+  return new Date().toISOString().slice(0, 10);
 }
